bd9cab6176d155c10616efd8a7f6b98fa43f6f5e
mayuresh <mayuresh9999@gmail.com>
2025-07-22 12:23:22 +0530

    updated salary in sqlConnector2

--- SqlJavaConnection/src/main/java/org/example/config/SqlConnector.java
+++ SqlJavaConnection/src/main/java/org/example/config/SqlConnector.java
@@ -5,15 +5,28 @@ import java.sql.*;
 public class SqlConnector {
 
 
-    public Connection getConnection() throws SQLException {
+    public static void main(String[] args) throws Exception {
         String url = "jdbc:mysql://localhost:3306/testdb";
         String username = "root";
         String pass = "root123";
         String fetchData = "select * from testdb.staff";
         Connection connection = DriverManager.getConnection(url, username, pass);
         Statement statement = connection.createStatement();
-        //int a = statement.execute("");
-        ResultSet rs = statement.executeQuery(fetchData);
-        return connection;
+//int a = statement.execute("");
+        ResultSet rs = statement.executeQuery("select * from staff where id = 1");
+        while
+        (rs.next()) {
+            int id = rs.getInt("id");
+            String firstname = rs.getString("first_name");
+            String lastname = rs.getString("last_name");
+            int mobile = rs.getInt("mobile");
+            String email = rs.getString("email");
+            String city = rs.getString("city");
+            System.out.println(id + " : " + firstname + " : " + lastname + " : " + mobile + " : " + email + " : " + city + " : ");
+
+        }
     }
+
 }
+
+
